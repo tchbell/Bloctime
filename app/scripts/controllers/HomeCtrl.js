9446879c864f957ced4d8cfac35205f92494038f
Ted Bell <tchbell@gmail.com>
2017-07-11 12:53:58 -0400

    Working on showing timer

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,49 @@
 (function(){
-    function HomeCtrl(){
+    function HomeCtrl($interval){
         
+        this.fiveMin = 300000;
+        this.timer = null;
+        this.pomodoro = 0;
+        var time = 5000;
+        
+        var format = function(time){
+            var makeToSeconds = time/1000;
+            var seconds = Math.floor(makeToSeconds%60);
+            var minutes = Math.floor(makeToSeconds / 60);
+                
+            if(seconds < 10){
+                return  minutes + ":0" + seconds
+            }else{
+                return minutes + ":" + seconds
+            }
+        }
+        
+        this.startTimer = function(){
+            //var time = 5000;
+            this.message = "Timer Started " + time;
+                   this.timer = $interval(
+//                       function(){
+//                        time -=1000;
+//                        this.message = format(time);
+//                        console.log(this.message);
+//                    }
+                     updateTimer()  , 1000)
+
+        }
+        
+        var updateTimer = function() {
+            time -=1000;
+            this.message = format(time);
+            console.log(this.message);
+        }
+        
+        this.stopTimer = function(){
+            this.message = "Timer Stopped";
+            $interval.cancel(this.timer);
+        }    
     }
     
     angular
-        .module('Bloctime')
-        .controller('HomeCtrl', [HomeCtrl]);
-})
+        .module("Bloctime")
+        .controller("HomeCtrl", ["$interval", HomeCtrl]);
+})();
---
+++ app/scripts/services/Timer.js
@@ -0,0 +1,21 @@
+(function(){
+    function Timer($interval){
+        
+        
+        
+       
+        return {
+            twoFive: function(){
+                1,500,000 Milliseconds
+            },
+            five: function(){
+                300,000 Milliseconds
+            }
+            
+    }
+    
+    
+    angular
+        .module('Blocitoff')
+        .factory('Timer', ['$timeout', '$interval', Task])
+})();

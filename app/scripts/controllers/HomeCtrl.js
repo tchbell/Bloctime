5add2a38e663eeb9de4501a62d3526aeb32a941b
Ted Bell <tchbell@gmail.com>
2017-07-14 14:03:04 -0400

    Added pomodoro counter to have longer break at 4th interval. AAlso disabled session button after the start work timer is called until rest timer runs

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,25 +1,96 @@
 (function(){
     function HomeCtrl(Timer, $interval){
-
-        this.timerOn = false;
+        var pomodoro = 2;
+        var onBreak = false;
         
+        
+        
+        
+        //config telling if timer is running or not
+        //not final
+        this.timerOn = function(){
+           return Timer.isOn;
+        };
+        
+        this.timer = function(){
+            if(Timer.isOn === true){
+                Timer.isOn = false;
+            }else{
+                Timer.isOn = true;
+            }
+            console.log(this.timerOn());
+        };
+        
+       
+        //starting work and rest timer
+        //works but might edit later
+        
+        //Instructions
+        this.startSession = "Start Here";
+        this.sessionRest = "";
         
         var startTimer = function(time){
-            this.timerOn = true;
-            this.message = "Timer has started";
             Timer.timerStart(time);
-
-            }
+            };
+        
+        this.stopTimer = function(){
+            Timer.timerStop();
+            this.message = "Timer has stopped";
+            pomodoro = 0;
+            
+        } 
         
         this.workTimer = function(){
+            pomodoro++;
             startTimer(3000);
-        }
+            if(pomodoro < 4){
+                this.startSession = "";
+                this.sessionRest = "Once timer completes click here to start rest period";
+                onBreak = true;
+            }else{
+                this.startSession = "";
+                this.sessionRest = "Click here to start your long rest break";
+            }
+        };
+        
+        
         
         
         this.restTimer = function(){
-            startTimer(2000);
-            
+            if(pomodoro === 4){
+                startTimer(5000);
+                onBreak = false;
+                pomodoro = 0;
+            }else{
+                startTimer(2000);
+                onBreak = false;
+            }
+            this.startSession = "Now click here to start another work session";
+            this.sessionRest = "";
         };
+        
+        //disable button method
+        this.checkForBreak = function(){
+            if(onBreak === true){
+            return true;
+            }
+        }
+        
+        this.disable = function(){
+            if(onBreak === true || this.timerOn() === true){
+                return true;
+            }
+        }
+        
+         
+        
+        
+        //showing timer
+        //needs work
+        this.getMessage = function(){
+            return Timer.message;
+            console.log(Timer.message);
+        }
         
 
         
@@ -29,12 +100,7 @@
 //            console.log(this.message);
 //        }
         
-        this.stopTimer = function(){
-            Timer.timerStop();
-            this.timerOn = false;
-            this.message = "Timer has stopped";
-            
-        }    
+          
     }
     
     angular
--- app/scripts/services/Timer.js
+++ app/scripts/services/Timer.js
@@ -1,42 +1,59 @@
-(function(){
-    function Timer($interval){
+(function () {
+    function Timer($interval) {
         
         var timer = null;
         
-        var format = function(time){
-            var makeToSeconds = time/1000;
-            var seconds = Math.floor(makeToSeconds%60);
+        var format = function (time) {
+            var makeToSeconds = time / 1000;
+            var seconds = Math.floor(makeToSeconds % 60);
             var minutes = Math.floor(makeToSeconds / 60);
                 
-            if(seconds < 10){
-                return  minutes + ":0" + seconds
-            }else{
-                return minutes + ":" + seconds
+            if (seconds < 10) {
+                return minutes + ":0" + seconds;
+            } else {
+                return minutes + ":" + seconds;
             }
-        }
+        };
+    
         
         return {
             
-                timerStart: function(time){
-                    this.timerOn = true;
-                           timer = $interval(
-                                       function(){
-                                           if(time > 0){
-                                                time -=1000;
-                                                console.log(format(time));
-                                           }else{
-                                               $interval.cancel(timer);
-                                           }
-                                       }, 1000)
-
-                },
+            isOn: false,
             
-                timerStop: function(){
-                    $interval.cancel(format(timer));
-                }
+            turnOn: function(){
+                Timer.isOn = true;
+            },
+            
+            timerStart: function (time) {
+            // Console says its not a function
+             //   Timer.turnOn();
+                timer = $interval(
+                    function () {
+                        Timer.isOn = true;
+                        console.log(Timer.isOn);
+                        if (time > 0) {
+                            Timer.message = "Timer has started " + format(time);
+                            console.log(Timer.message);
+                            time -= 1000;
+                        } else {
+                            $interval.cancel(timer);
+                            Timer.message = "Timer completed";
+                            console.log(Timer.message);
+                            Timer.isOn = false;
+                        }
+                    },1000);
+            },
             
             
+            
+            timerStop: function () {
+                $interval.cancel(timer);
+                Timer.isOn = false;
+                console.log(Timer.message);
             }
+            
+            
+        };
   
     
     }

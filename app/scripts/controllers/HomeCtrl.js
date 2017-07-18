9b0797d16b39b2afedd52b9709483ab1b09f9e62
Ted Bell <tchbell@gmail.com>
2017-07-18 15:03:56 -0400

    Moved timer function out of service and into controller, live timer now shows proper time

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,57 +1,100 @@
-(function(){
-    function HomeCtrl(Ding, Timer, $interval){
+(function () {
+    function HomeCtrl(Ding, $interval) {
+        var timer = null;
         var pomodoro = 2;
         var onBreak = false;
-        const WORKSESSION = 1500000;
-        const BREAKSESSION = 300000;
-        const LONGBREAK = 1800000;
+        var WORKSESSION = 1500000;  //25min
+        var BREAKSESSION = 300000;  //5min
+        var LONGBREAK = 1800000;    //30min
+        this.message = "";
         
-        
-        
-        
-        //config telling if timer is running or not
-        //still editing
-        this.timerOn = function(){
-           return Timer.isOn;
+        //Alert Noise
+        var ding = function () {
+            return Ding.noisePlay();
         };
         
-        this.timer = function(){
-            if(Timer.isOn === true){
-                Timer.isOn = false;
-            }else{
-                Timer.isOn = true;
+        //Prevents multiple timers running
+        var timerOn = false;
+        
+        this.disable = function () {
+            if (onBreak || timerOn) {
+                return true;
             }
-          //  console.log(this.timerOn());
         };
         
-       
-        //starting work and rest timer
-        //works but might edit later
+        this.timerRunning = function () {
+            if (timerOn) {
+                return true;
+            }
+        };
+
+
         
+        //Timer Code
         //Instructions
         this.startSession = "Start Here";
         this.sessionRest = "";
+        var timerVal = "0:00";
+        this.showTimerVal = function () {
+            return timerVal;
+        };
+        
+        //Make the miliseconds more readable
+        
+        var format = function (time) {
+            var makeToSeconds = time / 1000;
+            var seconds = Math.floor(makeToSeconds % 60);
+            var minutes = Math.floor(makeToSeconds / 60);
+                
+            if (seconds < 10) {
+                return minutes + ":0" + seconds;
+            } else {
+                return minutes + ":" + seconds;
+            }
 
+        };
         
-        var startTimer = function(time){
-            Timer.timerStart(time);
+
+        var timerStart = function (time) {
+                timer = $interval(
+                    function () {
+                        if (time > 0) {
+                            time -= 1000;
+                            timerVal = format(time);
+                        } else {
+                            ding();
+                            timerOn = false;
+                            this.message = "Timer has ended";
+                            $interval.cancel(timer);
+                        }
+                }, 1000);
             };
+
+        //starting work and rest timer
+        var startTimer = function (time) {
+            timerStart(time);
+            timerOn = true;
+        };
         
-        this.stopTimer = function(){
-            Timer.timerStop();
+        this.stopTimer = function () {
             this.message = "Timer has stopped";
+            $interval.cancel(timer);
+            timerVal = "0:00";
+            timerOn = false;
+            onBreak = false;
             pomodoro = 0;
             
-        } 
+        };
         
-        this.workTimer = function(){
+        this.workTimer = function () {
             pomodoro++;
+            this.message = "Begin work!";
             startTimer(3000);
-            if(pomodoro < 4){
+            if (pomodoro < 4) {
                 this.startSession = "";
                 this.sessionRest = "Once timer completes click here to start rest period";
                 onBreak = true;
-            }else{
+            } else {
                 this.startSession = "";
                 this.sessionRest = "Click here to start your long rest break";
             }
@@ -60,58 +103,23 @@
         
         
         
-        this.restTimer = function(){
-            if(pomodoro === 4){
+        this.restTimer = function () {
+            if (pomodoro === 4) {
+                this.message = "Rest has begun";
                 startTimer(5000);
                 onBreak = false;
                 pomodoro = 0;
-            }else{
+            } else {
                 startTimer(2000);
                 onBreak = false;
             }
             this.startSession = "Now click here to start another work session";
             this.sessionRest = "";
         };
-        
-        //disable button method
-        this.checkForBreak = function(){
-            if(onBreak === true){
-            return true;
-            }
-        }
-        
-        this.disable = function(){
-            if(onBreak === true || this.timerOn() === true){
-                return true;
-            }
-        }
-        
-         
-        
-        
-        //showing timer
-        //needs work
-        this.getMessage = function(){
-        }
-        
-        this.message = function(){
-        
-        }
-        
 
-
-
-        
-//        var updateTimer = function() {
-//            time -=1000;
-//            this.message = format(time);
-//            console.log(this.message);
-//        }
-        
-          
     }
     
     angular
         .module("Bloctime")
-        .controller("HomeCtrl", ["Ding", "Timer", "$interval", HomeCtrl]);
+        .controller("HomeCtrl", ["Ding", "$interval", HomeCtrl]);
 })();
